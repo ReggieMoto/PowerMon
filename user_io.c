2ac445054471b3686e857b289da9b6365f17319f
David Hammond <git@davidhammond.net>
2017-08-05 15:48:55 -0400

    First rev of I/O files.

---
+++ user_io.c
@@ -0,0 +1,19 @@
+* =================================
+ * user_io.c
+ */
+
+#include <stdio.h>
+
+ void user_io_thread(void)
+{
+
+	int thread_exit = FALSE;
+
+	printf("Entering PowerMon: user_io_thread.");
+
+	do {
+
+	} while (!thread_exit):
+
+	printf("Exiting PowerMon: user_io_thread.")
+}
---
+++ wifi_io.c
@@ -0,0 +1,19 @@
+* =================================
+ * wifi_io.c
+ */
+
+#include <stdio.h>
+
+ void wifi_io_thread(void)
+{
+
+	int thread_exit = FALSE;
+
+	printf("Entering PowerMon: wifi_io_thread.");
+
+	do {
+
+	} while (!thread_exit):
+
+	printf("Exiting PowerMon: wifi_io_thread.")
+}
